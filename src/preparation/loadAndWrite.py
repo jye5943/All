082d02642daaa88d2490ae886e6670824595d8d6
kimhyokwan <haiteam@naver.com>
2019-04-22 00:07:59 +0900

    .

--- src/preparation/loadAndWrite.py
+++ src/preparation/loadAndWrite.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup #Used to read XML table on webpage
 import pandas as pd
 from common import cFunction as cf
 import numpy as np
+import wget
 
 # get dataList from filesystem to load and write
 #dataList = pd.read_excel("../../data/inbound/dataList.xlsx")
@@ -16,6 +17,11 @@ print("### The total number of target data is " + str(len(dataList)))
 dataList = dataList[   dataList['사이트'].notnull() ]
 print(dataList[["사이트"]])
 print("### The total number of filtered data is " + str(len(dataList)))
+
+###################################################
+# Filtering -> for your own object
+#dataList = dataList[   dataList['번호'] == "352" ]
+###################################################
 
 # create folder to save result
 outPath = "../../data/outbound/"
@@ -35,6 +41,8 @@ for dataCount in range(0,len(dataList)):
     inputFolder = dataList.loc[dataCount, "폴더명"]
     inputFile = dataList.loc[dataCount, "서비스명"]
     inputDataType = dataList.loc[dataCount, "데이터타입"]
+    inputRefUrl = dataList.loc[dataCount, "참고문서"]
+    inputRefType = dataList.loc[dataCount, "참고문서타입"]
     print(inputUrl)
 
     url = cf.makeURL(inputUrl,inputKey,inputParameter)
@@ -55,3 +63,11 @@ for dataCount in range(0,len(dataList)):
         newDF.to_csv(fullOutPath, index=False, encoding="ms949")
     except Exception as x:
         print(x)
+
+    fullOutRefPath = outPath + inputFolder + "/" + inputFolder + inputFile + "."+inputRefType
+    try:
+        wget.download(inputRefUrl, fullOutRefPath)
+    except Exception as e:
+        print(inputFolder+"참고문서 Error")
+        print(e)
+        pass
